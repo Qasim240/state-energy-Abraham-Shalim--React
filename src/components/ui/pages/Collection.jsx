36aad20a3dbd1cf2ebca53c14e4b0748b6f0f1b1
Muhammad Qasim <muhammadqasim1014@gmail.com>
2025-06-04 23:34:06 +0500

    complete the collection page figma conversion fully responsive

--- src/components/ui/pages/Collection.jsx
+++ src/components/ui/pages/Collection.jsx
@@ -1,24 +1,66 @@
 import React from 'react'
 import CollectionCard from '../CollectionCard'
+import { collectionImg, door, roof, solarPenels, installatioBanner, heater } from '../../../../imagesPath'
 
 const Collection = () => {
+
+    const Collections = [
+        {
+            title: "HVAC",
+            collectionImg: collectionImg
+        },
+        {
+            title: "SOLAR",
+            collectionImg: solarPenels
+        },
+        {
+            title: "ROOF",
+            collectionImg: roof
+        },
+        {
+            title: "DOORS",
+            collectionImg: door
+        },
+        {
+            title: "Insulation",
+            collectionImg: installatioBanner
+        },
+        {
+            title: "Water Heater",
+            collectionImg: heater
+        },
+         {
+            title: "DOORS",
+            collectionImg: door
+        },
+        {
+            title: "HVAC",
+            collectionImg: collectionImg
+        },
+    ]
+
+
     return (
 
-
-<div class="grid lg:grid-cols-4 grid-cols-2 gap-4">
-       <CollectionCard />
-       <CollectionCard />
-       <CollectionCard />
-       <CollectionCard />
-</div>
-
+        <div class="grid lg:grid-cols-4 grid-cols-2 gap-4">
+            {Collections.map((collection, index) => (
+                <div key={index}>
+                    <CollectionCard title={collection.title} thumbnailImg={collection.collectionImg} />
+                </div>
+            ))}
 
 
 
 
+        </div>
 
 
-     
+
+
+
+
+
+
     )
 }
 
